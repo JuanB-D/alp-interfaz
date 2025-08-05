294534ec245e97975353d7ffd0d1bd208bd610cc
Mathesito <109560590+ImMatthewDev@users.noreply.github.com>
2025-08-04 22:30:48 -0500

    Update notes.js

--- api/notes.js
+++ api/notes.js
@@ -1,37 +1,27 @@
-const cors = require('cors');
-const express = require('express');
-const app = express();
 const { studentsDB } = require('./data');
 
-app.use(cors());
-app.use(express.json());
-
-const router = express.Router();
-
-router.post('/', (request, response) => {
-    const { studentName, note, className } = request.body;
-    
-    if (studentsDB[studentName]) {
-        studentsDB[studentName].notes.push(note);
+module.exports = async (request, response) => {
+    if (request.method === 'POST') {
+        const { studentName, note, className } = request.body;
         
-        const total = studentsDB[studentName].notes.reduce((sum, current) => sum + current, 0);
-        const newAverage = (total / studentsDB[studentName].notes.length).toFixed(1);
-        studentsDB[studentName].average = parseFloat(newAverage);
-        
-        console.log(`Asignando nota: ${note} a ${studentName}. Nuevo promedio: ${newAverage}`);
-        
-        return response.status(200).json({ 
-            success: true, 
-            message: `Nota ${note} asignada a ${studentName}`,
-            newAverage: newAverage 
-        });
+        if (studentsDB[studentName]) {
+            studentsDB[studentName].notes.push(note);
+            
+            const total = studentsDB[studentName].notes.reduce((sum, current) => sum + current, 0);
+            const newAverage = (total / studentsDB[studentName].notes.length).toFixed(1);
+            studentsDB[studentName].average = parseFloat(newAverage);
+            
+            console.log(`Asignando nota: ${note} a ${studentName}. Nuevo promedio: ${newAverage}`);
+            
+            response.status(200).json({ 
+                success: true, 
+                message: `Nota ${note} asignada a ${studentName}`,
+                newAverage: newAverage 
+            });
+        } else {
+            response.status(404).json({ success: false, message: 'Estudiante no encontrado' });
+        }
     } else {
-        return response.status(404).json({ success: false, message: 'Estudiante no encontrado' });
+        response.status(405).send('Método no permitido');
     }
-});
-
-// Exportación para Vercel
-module.exports = (request, response) => {
-    app.use('/', router);
-    app(request, response);
 };
